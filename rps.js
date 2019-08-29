62c495df048aeb13c6dd5889a258b463523fd8e7
Emichu <k.kreplewicz@gmail.com>
2019-08-29 19:15:11 +0100

    establishing the basics

--- rps.js
+++ rps.js
@@ -1,3 +1,10 @@
 var prompt = require('prompt-sync')();
-var answer = prompt("what's the meaning of life?");
-console.log(answer);
+
+var answer = prompt("rock, paper, or scissors?")
+
+
+if(answer === "rock" || answer === "paper" || answer === "scissors"){
+    console.log("You chose " + answer)
+} else {
+    console.log("Sorry, didn't recognise your choice")
+}
